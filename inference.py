0c25588ef5690356ba1faf2783b8bf3454cce000
rlaalsxo <alsxo124@naver.com>
2025-11-13 19:37:20 +0900

    Update inference.py

--- inference.py
+++ inference.py
@@ -483,13 +483,13 @@ def main(config):
         )
 
     # Amber relaxation (옵션)
-    # if config.relax_steps is not None:
-    #     relax_steps = int(config.relax_steps)
-    #     if relax_steps > 0:
-    #         with timing(f'Amber Relaxation : {relax_steps} iterations', logger=logger):
-    #             amber_relax = AmberRelaxation(max_iterations=relax_steps, logger=logger)
-    #             relaxed_model = f'{config.output_dir}/relaxed_{relax_steps}_model.pdb'
-    #             amber_relax.process(unrelaxed_model, relaxed_model)
+    if config.relax_steps is not None:
+        relax_steps = int(config.relax_steps)
+        if relax_steps > 0:
+            with timing(f'Amber Relaxation : {relax_steps} iterations', logger=logger):
+                amber_relax = AmberRelaxation(max_iterations=relax_steps, logger=logger)
+                relaxed_model = f'{config.output_dir}/relaxed_{relax_steps}_model.pdb'
+                amber_relax.process(unrelaxed_model, relaxed_model)
 
 
 if __name__ == '__main__':
